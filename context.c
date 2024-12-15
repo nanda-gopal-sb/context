21be45008f6df643f32108ae7e1fcfe938708434
Nandagopal <nandagopal.nsb@gmail.com>
2024-12-15 09:29:21 +0530

    Added

--- context.c
+++ context.c
@@ -1,7 +1,10 @@
 #include "stdio.h"
-
+#include <unistd.h>
 int main(int argc, char const *argv[])
 {
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1)
+        ;
     printf("Hello\n");
     return 0;
 }
